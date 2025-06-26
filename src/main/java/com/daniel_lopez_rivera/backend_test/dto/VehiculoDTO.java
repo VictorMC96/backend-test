f0c3eff2080e795bc8122bf1360f1bc2630b379f
GitLopez95 <isc.lopez95@gmail.com>
2025-06-25 22:13:42 -0600

    Propuesta de solución a prueba técnica.

--- src/main/java/com/daniel_lopez_rivera/backend_test/dto/VehiculoDTO.java
+++ src/main/java/com/daniel_lopez_rivera/backend_test/dto/VehiculoDTO.java
@@ -1,8 +1,5 @@
 package com.daniel_lopez_rivera.backend_test.dto;
 
-import com.daniel_lopez_rivera.backend_test.util.VehiculoTipo;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.Data;
 
 @Data
--- src/main/java/com/daniel_lopez_rivera/backend_test/repository/VehiculoRepository.java
+++ src/main/java/com/daniel_lopez_rivera/backend_test/repository/VehiculoRepository.java
@@ -3,7 +3,6 @@ package com.daniel_lopez_rivera.backend_test.repository;
 import com.daniel_lopez_rivera.backend_test.model.Vehiculo;
 import com.daniel_lopez_rivera.backend_test.util.VehiculoTipo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
